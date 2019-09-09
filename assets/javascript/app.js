02b397e44630453821b39f2c72e8534370ea291f
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-09-09 00:51:15 -0700

    added timer functionality, added guess detection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,9 @@ var num = 0;
 var triviaArr = [];
 var answerArr = [];
 var answerID = 0;
+var correctAnswer;
+var wins = 0;
+var loss = 0;
 
 // function genID() {
 //   $.ajax({
@@ -22,48 +25,107 @@ var answerID = 0;
 //   });
 // } genID();
 
+
+// powered by: https://opentdb.com/
+
 function genTrivia() {
   let queryURL = `https://opentdb.com/api.php?amount=10&category=15`;
 
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
+    // success: askQuestion()
   }).then((response) => {
     for (let i=0; i < response.results.length; i++)
     triviaArr.push(response.results[i])
   });
-} genTrivia()
+}
 
-// function startGame() {
+  genTrivia();
   timeReset();
+
   triviaArr.length = 0; // makes triviaArr ready for new inputs
   console.log(triviaArr)
 
   function askQuestion() {
+    var numChoice = 1;
+
     $('.questionBlock').append(`<p>${triviaArr[num].question}</p>`)
 
       answerArr.push(triviaArr[num].correct_answer);
       for (let j=0; j < triviaArr[num].incorrect_answers.length; j++) {
         answerArr.push(triviaArr[num].incorrect_answers[j]);
       } console.log(answerArr)
+      correctAnswer = answerArr[0];
       if (answerArr[0] === "True") {
-        $('.answerBlock').append(`<p class="choiceBlock">${answerArr[0]}</p>`)
-        $('.answerBlock').append(`<p class="choiceBlock">${answerArr[1]}</p>`)
+        $('.answerBlock').append(`<p class="choiceBlock" id="choice1" value="True" value="correct">${answerArr[0]}</p>`)
+        $('.answerBlock').append(`<p class="choiceBlock" id="choice2" value="False" value="incorrect">${answerArr[1]}</p>`)
       } else if (answerArr[1] === "True") {
-        $('.answerBlock').append(`<p class="choiceBlock">${answerArr[1]}</p>`)
-        $('.answerBlock').append(`<p class="choiceBlock">${answerArr[0]}</p>`)
+        $('.answerBlock').append(`<p class="choiceBlock" id="choice1" value="False" value="incorrect">${answerArr[1]}</p>`)
+        $('.answerBlock').append(`<p class="choiceBlock" id="choice2" value="True" value="correct">${answerArr[0]}</p>`)
       } else {
         do {
           answerID = Math.floor(Math.random() * answerArr.length);
-          $('.answerBlock').append(`<p class="choiceBlock">${answerArr[answerID]}</p>`)
+          $('.answerBlock').append(`<p class="choiceBlock" id="choice${numChoice}" value="${answerArr[answerID]}">${answerArr[answerID]}</p>`)
           answerArr.splice(answerID, 1)
+          numChoice++
         } while (answerArr.length > 0)
-      } 
+      }
+    clickChoice();
     num++
-  };
+  }
 
   $('#test').on('click', nextQuestion);
-//  
+
+function clickChoice() {  
+  $('#choice1').on('click', () => {
+    console.log($('#choice1').attr("value"))
+    console.log(`Correct Answer: ${correctAnswer}`)
+    if ($('#choice1').attr("value") === correctAnswer){
+      console.log('Correct!')
+      wins++;
+      // win transition
+    } else {
+      console.log('Incorrect!')
+      loss++;
+      // loss transition
+    }
+  }); 
+  $('#choice2').on('click', () => {
+    console.log($('#choice2').attr("value"))
+    console.log(`Correct Answer: ${correctAnswer}`)
+    if ($('#choice2').attr("value") === correctAnswer){
+      console.log('Correct!')
+      wins++;
+    } else {
+      console.log('Incorrect!')
+      loss++;
+    }
+  }); 
+  $('#choice3').on('click', () => {
+    console.log($('#choice3').attr("value"))
+    console.log(`Correct Answer: ${correctAnswer}`)
+    if ($('#choice3').attr("value") === correctAnswer){
+      console.log('Correct!')
+      wins++;
+    } else {
+      console.log('Incorrect!')
+      loss++;
+    }
+  }); 
+  $('#choice4').on('click', () => {
+    console.log($('#choice4').attr("value"))
+    console.log(`Correct Answer: ${correctAnswer}`)
+    if ($('#choice4').attr("value") === correctAnswer){
+      console.log('Correct!')
+      wins++;
+    } else {
+      console.log('Incorrect!')
+      loss++;
+    }
+  }); 
+}
+
 
 function nextQuestion() {
   answerArr.length = 0; //clears array
@@ -77,7 +139,7 @@ function nextQuestion() {
 }
 
 var clockRunning = false;
-var time = 5;
+var time = 30;
 var intervalID;
 
 function timeStart() {
@@ -91,7 +153,8 @@ function timeStop() {
   clockRunning = false;
 }
 function timeReset() {
-  time = 5;
+  timeStop();
+  time = 30;
   $('#timer').text(`${time}`);
 }
 
@@ -102,7 +165,9 @@ function count() {
     timeStop();
   }
 }
-
+$('#timerTest').on('click', timeStart);
+$('#timerTest2').on('click', timeStop);
+$('#timerTest3').on('click', timeReset);
 
 
 
